4dd3b823c8a0f05a6f631079e003d40ae103cf33
Rita Leppiniemi <ritaleppiniemi@gmail.com>
2023-02-03 13:23:31 +0200

    Initial Commit

---
+++ test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Rita Leppiniemi on 3.2.2023.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
